a383815754b443ae261aba101893bde728c75c9d
pedro <zpedroluis@outlook.com>
2020-02-10 13:53:24 -0300

    feat: add search term and prefix

---
+++ index.js
@@ -0,0 +1,27 @@
+const readLine = require('readline-sync')
+function start()
+{
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm()
+    {
+        return readLine.question("Type a Wikipedia search term: ")
+    }
+
+    function askAndReturnPrefix()
+    {
+        const prefixes = ['Who is ','What is', 'The history of']
+        const selectedPrefixIndex = readLine.keyInSelect(prefixes)
+        const selectedPrefixText = prefixes[selectedPrefixIndex]  
+        console.log(selectedPrefixText)
+
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
